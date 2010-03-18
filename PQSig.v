a61d0a94ec25bdc2f61924980be9cfe5844eb4b8
Jim Apple <mercurial@jbapple.com>
2010-03-17 20:06:24 -0700

    New specification in separate module

--- PQSig.v
+++ PQSig.v
@@ -4,7 +4,7 @@ Module Type PQSig.
 
   Declare Module O:Order.
   
-  Import O.
+  Export O.
 
   Parameter PQ:Type.
 
@@ -15,63 +15,79 @@ Module Type PQSig.
   Parameter deleteMin : PQ -> PQ.
   Parameter meld : PQ -> PQ -> PQ.
 
-  Parameter In : A -> PQ -> Prop.
-  Parameter Size : PQ -> nat -> Prop.
-
-  Parameter inTotal :
-    forall x p,
-      In x p \/ ~ In x p.
-
-  Parameter sizeTotal :
-    forall x,
-      exists n,
-        Size x n.
-
-  Parameter sizeFunction :
-    forall x n m,
-      Size x n ->
-      Size x m ->
-      n = m.
-
-  Parameter emptyIn : forall x, ~ In x empty.
-  Parameter emptySize : Size empty 0.
-
-  Parameter insertIn : 
-    forall y x p, 
-      In y (insert x p)
-      <->
-      (In y p \/ y = x).
-  Parameter insertSize : 
-    forall p n, Size p n <-> 
-      forall x, Size (insert x p) (S n).
-
-  Parameter findMinIn : 
-    forall x p, 
-      findMin p = Some x <-> 
-      (In x p /\ forall y, In y p -> O.LEQ x y = true).
-  Parameter findMinSize : forall p, findMin p = None <-> Size p 0.
-
-  Parameter deleteMinIn :
-    forall p x,
-      findMin p = Some x <->
-      (forall y, In y p <->
-        In y (insert x (deleteMin p))).
-  Parameter deleteMinSizeS : 
-    forall p n, 
-      Size p (S n) -> Size (deleteMin p) n.
-  Parameter deleteMinSizeZ :
-    forall p,
-      Size p 0 ->
-      Size (deleteMin p) 0.
-
-  Parameter meldIn :
-    forall p q x,
-      In x (meld p q) <->
-      (In x p \/ In x q).
-  Parameter meldSize :
-    forall p q n m,
-      Size p n ->
-      Size q m ->
-      Size (meld p q) (n+m).
-
 End PQSig.
+
+Definition DER a leq:= 
+  {der : a -> a -> bool |
+    (forall x y, true = der x y -> 
+      ~(true = leq x y /\ true = leq y x))
+    /\ (forall x, der x x = true)
+    /\ (forall x y, der x y = der y x)
+    /\ (forall x y z, 
+      true = der x y ->
+      true = der y z ->
+      true = der x z)}.
+
+Module Type PQVerify.
+
+  Declare Module PQS:PQSig.
+
+  Export PQS.
+
+  Definition SAME := DER A LEQ.
+
+  Parameter count : SAME -> A -> PQ -> nat.
+
+  Definition check (same:SAME) x y := 
+    match same with
+      | exist f _ => f x y
+    end.
+
+  Parameter countSAME :
+    forall same x y,
+      check same x y = true ->
+      forall inp, count same x inp = count same y inp.
+
+  Parameter emptyCount :
+    forall same x, count same x empty = 0.
+
+  Parameter insertCount :
+    forall same x inp,
+      let oldCount := count same x inp in
+        forall y,
+          count same x (insert y inp) =
+          if check same x y 
+            then S oldCount
+            else oldCount.
+
+  Parameter findMinCount :
+    forall inp,
+      match findMin inp with
+        | None => forall same x, count same x inp = 0
+        | Some x =>
+          forall same y, 
+            if check same y x
+              then count same y inp > 0
+              else count same y inp > 0 ->
+                LEQ x y = true
+      end.
+
+  Parameter deleteMinCount :
+    forall inp,
+      match findMin inp with
+        | None => forall same x, count same x (deleteMin inp) = 0
+        | Some x =>
+          forall same y, 
+            let newCount := count same y (deleteMin inp) in
+              count same y inp =
+              if check same y x
+                then S newCount
+                else newCount
+      end.
+
+  Parameter meldCount :
+    forall same inp inq x,
+      count same x (meld inp inq) = count same x inp
+                                  + count same x inq.
+
+End PQVerify.
